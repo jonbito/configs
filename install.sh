7e5cc14b6fce89b99af58da2b5177a34e8c8245b
Jonathan Bishop <r3tnull@gmail.com>
2023-04-09 08:17:24 -0400

    Add timewarrior

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-brew install alacritty ripgrep fish tmux lazygit bottom
+brew install alacritty ripgrep fish tmux lazygit bottom wget node task python timewarrior
+pip3 install pynvim tasklib six packaging
 mkdir -p ~/.config/fish
 mkdir -p ~/.config/nvim
+mkdir -p ~/.task/hooks
+cp /opt/homebrew/Cellar/timewarrior/1.5.0/share/doc/timew/ext/on-modify.timewarrior ~/.task/hooks/
+chmod +x ~/.task/hooks/on-modify.timewarrior
 if [[ ! -e ~/.config/nvim ]]; then
-  git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+	git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
 fi
 cp ./.tmux.conf ~/
 cp ./.gitconfig ~/
