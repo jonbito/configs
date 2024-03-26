9904b851212a8a611bc591404e1ebc686b8178d0
Jonathan Bishop <r3tnull@gmail.com>
2024-03-26 17:04:16 -0400

    use zsh

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-brew install alacritty ripgrep fish tmux bottom wget node python go rust rust-analyzer luarocks xwmx/taps/nb
+brew install alacritty ripgrep tmux bottom wget node python go rust rust-analyzer xwmx/taps/nb
 nb set editor nvim
-mkdir -p ~/.config/fish
 cp ./.tmux.conf ~/
 cp ./.gitconfig ~/
-cp ./.config/fish/config.fish ~/.config/fish/
-cp ./.config/alacritty/alacritty.yml ~/.config/alacritty/
+cp ./.zshrc ~/
+cp ./.p10k.zsh ~/
+cp ./.config/alacritty/alacritty.toml ~/.config/alacritty/
 
 rm -rf ~/.config/nvim
 cp -r ./.config/nvim ~/.config/nvim
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth 1 -- https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
